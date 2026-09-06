ec13f9dc2aa8c190e11190fe5801accb0c0ff199
Mohamad Farhan <poseiden2002@gmail.com>
2026-09-07 00:56:52 +0800

    gaddamnit, retest if cloud server is running this version

--- somethic/index.php
+++ somethic/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/config/database.php';
-
-$page_title = 'testetesasdanwoiSOMETHIC — Handbag Retail Boutique';
+    echo $pdo->getAttribute(PDO::ATTR_DRIVER_NAME);
+$page_title = 'ni jadi en? SOMETHIC — Handbag Retail Boutique';
 require_once __DIR__ . '/includes/header.php';
 
 // Fetch 4 featured handbags
