ed940e6bf1c5fe2c779eb4ee2cff30c9c0c3e6ed
Mohamad Farhan <poseiden2002@gmail.com>
2026-09-06 23:58:43 +0800

    test nama

--- somethic/index.php
+++ somethic/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/config/database.php';
 
-$page_title = 'SOMETHIC — Handbag Retail Boutique';
+$page_title = 'testetesasdanwoiSOMETHIC — Handbag Retail Boutique';
 require_once __DIR__ . '/includes/header.php';
 
 // Fetch 4 featured handbags
